fdcf3e35b1dd0ab1ac87316eef2403b4f5d65e46
Ahmad-AlModhayan <80857023+Ahmad-AlModhayan@users.noreply.github.com>
2021-06-28 20:23:21 +0300

    update train.py

--- functions_train.py
+++ functions_train.py
@@ -1,9 +1,11 @@
 import torch
 from torchvision import transforms, datasets
 
-
 ####################################################
 # Function of transforms
+from train import args
+
+
 def train_data_load(train_dir):
     train_transforms = transforms.Compose([
         transforms.RandomResizedCrop(size=224),
@@ -16,6 +18,7 @@ def train_data_load(train_dir):
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
 
     return train_loader
+
 
 ####################################################
 # Function of transforms valid&test
@@ -32,5 +35,59 @@ def valid_test_data_load(test_dir):
 
     return test_loader
 
-####################################################
 
+####################################################
+# Validation Function
+def validation(model, valid_loader, criterion):
+    valid_loss = 0
+    accuracy = 0
+
+    for inputs, labels in valid_loader:
+        inputs, labels = inputs.to('cuda'), labels.to('cuda')
+
+        output = model.forward(inputs)
+        valid_loss += criterion(output, labels).item()
+
+        props = torch.exp(output)
+        equality = (labels.data == props.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+
+    return valid_loss, accuracy
+
+
+# Train Function
+def train_classifier(model, optimizer, criterion, train_loader, valid_loader, args.epochs):
+    running_loss = 0
+    epochs = args.epochs
+    steps = 0
+    prints_every = 40
+
+    model.to('cuda')
+
+    for epoch in range(epochs):
+
+        model.train()
+
+        for inputs, labels in iter(train_loader):
+            inputs, labels = inputs.to('cuda'), labels.to('cuda')
+            steps += 1
+            optimizer.zero_grad()
+            output = model.forward(inputs)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % prints_every == 0:
+                model.eval()
+
+                with torch.no_grad():
+                    valid_loss, accuracy = validation(model, valid_loader, criterion)
+                print(f"Epoch {epoch + 1}/{epochs} (steps: {steps}).. "
+                      f"Train loss: {running_loss / len(train_loader):.3f}.. "
+                      f"Validation loss: {valid_loss / len(valid_loader):.3f}.. "
+                      f"Validation accuracy: {accuracy / len(valid_loader):.3f}.. ")
+
+                model.train()
+    print("!!Train of the model is finished!!")
--- train.py
+++ train.py
@@ -1,14 +1,50 @@
 import argparse
+import functions_train as f_t
+from torchvision import transforms, datasets, models
+import torch
+from torch import nn, optim
+from collections import OrderedDict
 
 parser = argparse.ArgumentParser(description='Build and Train the Model.')
-parser.add_argument("data_dir", )
-parser.add_argument("--save_dir")
-parser.add_argument("--arch")
-parser.add_argument("--learning_rate")
+parser.add_argument("data_dir", default='./flowers', help='Main directory')
+parser.add_argument("--save_dir", default='./checkpoint.pth')
+parser.add_argument("--arch", default='vgg16', help=' Model architecture')
+parser.add_argument("--learning_rate", type=float, default=0.001, help='Learning rate')
 parser.add_argument("--hidden_units")
-parser.add_argument("--epochs")
+parser.add_argument("--epochs", type=int, default=18, help='Number of epochs')
 parser.add_argument("--gpu")
 args = parser.parse_args()
 
+data_dir = args.data_dir
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
 
+train_loader = f_t.train_data_load(train_dir)
+valid_loader = f_t.valid_test_data_load(valid_dir)
+test_loader = f_t.valid_test_data_load(test_dir)
+
+# Architecture
+if args.arch == 'vgg16':
+    input_size = 25088
+    model = models.vgg16(pretrained=True)
+else:
+    print("This Model just supports vgg16 training")
+
+for parameter in model.parameters():
+    parameter.requires_grad = False
+
+# Initial_Classifier
+classifier = nn.Sequential(OrderedDict([
+    ('fc1', nn.Linear(input_size, 5000)),
+    ('relu', nn.ReLU()),
+    ('dropout', nn.Dropout(p=0.5)),
+    ('fc2', nn.Linear(5000, 120)),
+    ('output', nn.LogSoftmax(dim=1))]))
+model.classifier = classifier
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), args.learning_rate)
+
+# Classifier Training
+f_t.train_classifier(model, optimizer, criterion, train_loader, valid_loader)
 
